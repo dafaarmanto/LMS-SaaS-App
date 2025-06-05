f07a0fc90f667bb43e1c2723b53818d89730909a
Dafa Armanto <randikadafa07@gmail.com>
2025-06-05 15:15:26 +0700

    implement homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,47 @@
-import { Button } from "@/components/ui/button";
-import React from "react";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import { recentSessions } from "@/constants";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Hello Worldie</h1>
-      <Button>Lets get started</Button>
-    </div>
+    <main>
+      <h1 className="text-2xl underline">Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura, the Brainy Explorer"
+          topic="Neural network of the brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy, the Number Wizard"
+          topic="Derivatives and Integrals"
+          subject="maths"
+          duration={30}
+          color="#ffdaff"
+        />
+        <CompanionCard
+          id="789"
+          name="Verba, the Vocabulary Builder"
+          topic="English Literature"
+          subject="language"
+          duration={30}
+          color="#44da6e"
+        />
+      </section>
+      <section className="home-section">
+        <CompanionsList
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
   );
 };
 
